372f1b0aec39bac0ecd8fb5ca5c6ffd282df8aa3
waleed reda <walidreda428@gmail.com>
2021-06-21 23:00:16 +0200

    insert, getpost and getposts finished

--- main.go
+++ main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"demo/functions"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	
 	router := mux.NewRouter()
 	router.HandleFunc("/add", functions.InsertPost).Methods("POST")
 	router.HandleFunc("/posts/{kind}", functions.GetPost).Methods("GET")
 	router.HandleFunc("/posts", functions.GetPosts).Methods("GET")
-	http.ListenAndServe(":8088", router)
+	http.ListenAndServe(":"+port, router)
 }
